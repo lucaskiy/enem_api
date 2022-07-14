a218a1caca59b06f8b41ff24005c60264987a558
lucas <lucask.kiy@gmail.com>
2022-07-13 21:20:04 -0300

    added storage to bigquery method; added log print

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
 import pandas as pd
+import time
 import os
 from jsonschema import validate
 from utils.variables import schema, columns, final_columns, column_data_types
 from utils.gcloud_object import GcloudObject
+from utils.logger_print import print_log
 
 
 class ExtractEnem(GcloudObject):
@@ -10,47 +12,60 @@ class ExtractEnem(GcloudObject):
     def __init__(self):
         super().__init__()
         self.files_path = "./enem_files/"
-        self.parquet_files_path = "./parquet_files/"
+        self.parquet_files_path = "./parquet_files2/"
 
-
-    def read_csvs(self) -> None:
-        print("Starting process to read csv files and transform them into parquet!!")
+    def proccess_enem_files(self) -> None:
+        print(print_log("Starting process to read csv files and transform them into parquet!!"))
         files = os.listdir(self.files_path)
 
         for file in files:
             if file == ".gitignore":
                 continue
 
-            csv_files = str(self.files_path + file)
-            chunk_reader = pd.read_csv(csv_files, encoding="latin-1", sep=';', chunksize=100000)
+            csv_path = str(self.files_path + file)
+            self.read_csvs(file=file, csv_path=csv_path, chunksize=100000)
 
-            for i, chunk in enumerate(chunk_reader):
-                try:
-                    df_filename = f'{file.upper().strip(".CSV")}_part({i+1}).parquet'
-                    df = chunk.copy()
-                    df = df[columns]
-                    df = df.where(pd.notnull(df), -1)
-                    df = self.normalize_string_columns(df)
-                    df = self.data_treatment(df, file)
+        print(print_log(f"All raw enem files were saved to parquet format"))
+        time.sleep(5)
 
-                    valid_data = self.data_validation(df)
-                    if valid_data is True:
-                        df = df.replace({-1.0: None})
-                        df.columns = final_columns
-                        self.save_new_file_as_parquet(df, df_filename)
-                        
-                except Exception as error:
-                    print(f"Something went wrong while reading chunk -> {i}")
-                    raise error
+        save_data_to_bucket = self.upload_parquet_to_bucket()
+        if save_data_to_bucket:
+            time.sleep(5)
+            self.load_bucket_data_to_big_query()
 
-            print("All csv files were converted successfully!!")
+
+    def read_csvs(self, file:str, csv_path: str, chunksize=int) -> None:
+        print(print_log(f"Reading File -> {file}"))
+            
+        chunk_reader = pd.read_csv(csv_path, encoding="latin-1", sep=';', chunksize=chunksize)
+
+        for i, chunk in enumerate(chunk_reader):
+            try:
+                df_filename = f'{file.upper().strip(".CSV")}_part({i+1}).parquet'
+                df = chunk.copy()
+                df = df[columns]
+                df = df.where(pd.notnull(df), -1)
+                df = self.normalize_string_columns(df)
+                df = self.data_treatment(df, file)
+
+                valid_data = self.data_validation(df)
+                if valid_data is True:
+                    df = df.replace({-1.0: None})
+                    df.columns = final_columns
+                    self.save_new_file_as_parquet(df, df_filename)
+                    
+            except Exception as error:
+                print(print_log(f"Something went wrong while reading chunk -> {i}"))
+                raise error
+
+        print(print_log(f"{i} total files from {file} were converted from csv to parquet successfully!!"))
 
     def save_new_file_as_parquet(self, df: pd.DataFrame, df_filename: str) -> None:
         try:
             df.to_parquet(path=self.parquet_files_path + df_filename, index=False)
-            print(f"{df_filename} saved as parquet") 
+
         except Exception as error:
-            print(f"Something went wrong while converting {df_filename} to parquet")
+            print(print_log(f"Something went wrong while converting {df_filename} to parquet"))
             raise error
 
     @staticmethod
@@ -62,9 +77,8 @@ class ExtractEnem(GcloudObject):
             return df_copy
 
         except Exception as error:
-            print(f"Something went wrong while at the data treatment method on file - {file}")
+            print(print_log(f"Something went wrong while at the data treatment method on file - {file}"))
             raise error            
-
     
     @staticmethod
     def data_validation(df: pd.DataFrame) -> bool:
@@ -75,7 +89,7 @@ class ExtractEnem(GcloudObject):
                 validate(row, schema)
                 return True
             except Exception as error:
-                print(row)
+                print(print_log(row))
                 raise error
 
     @staticmethod
@@ -92,13 +106,13 @@ class ExtractEnem(GcloudObject):
 
         return df_copy
 
-    def upload_parquet_to_bucket(self):
-        print("Starting process to upload parquet files to GCP storage")
+    def upload_parquet_to_bucket(self) -> bool:
+        print(print_log("Starting process to upload parquet files to GCP storage"))
         files = os.listdir(self.parquet_files_path) 
         bucket = "files-etl"
 
         files_already_uploaded_to_bucket = self.get_files_inside_bucket(bucket)
-        count = 0
+        count = 1
 
         for file in files:
             try:
@@ -106,16 +120,19 @@ class ExtractEnem(GcloudObject):
                     continue
 
                 self.upload_to_bucket(blob_name=file, path_to_file=self.parquet_files_path+file, bucket_name=bucket)
-                print(f"Uploading file {count+1} of {len(files)} to bucket")
+                print(print_log(f"Uploading file {count} of {len(files)} to bucket"))
                 count += 1 
+                os.remove(self.parquet_files_path+file)  # delete parquet file after uploading it to bucket
+                time.sleep(1)
                 
             except Exception as error:
                 raise error
 
-        print("All files uploaded successfully!!")
+        print(print_log("All files uploaded successfully!!"))
+        return True
 
         
 if __name__ == '__main__':
     x = ExtractEnem()
-    x.read_csvs()
+    x.proccess_enem_files()
     
